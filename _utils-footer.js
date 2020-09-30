7ef1f881bde14ab9ff80ac5ac396747934c7d77f
araceli.cueva.contractor <araceli.cueva.contractor@bbva.com>
2020-09-30 10:14:42 -0500

    update document

---
+++ _utils-footer.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const CONTENT_ACCORDION_FOOTER_BENEFITS = {
+  title: 'Solicitar Tarjeta de crédito online BBVA',
+  opened: false,
+  items: [
+    {type: 'text-normal',data: 'Solicita tu Tarjeta de Crédito online BBVA desde la comodidad de tu hogar, desde tu trabajo o donde quiera que estés y en cualquier momento del día. Pide tu tarjeta de crédito online y empieza a disfrutar de todos los beneficios que tenemos para ti:'},
+    {type: 'lists-items',data: [{text: 'Descuentos en miles de establecimiento'},{text: 'Compra en cuotas y realiza tus pagos sin intereses en miles de tiendas'},{text: 'Promociones especiales'}]},
+    {type: 'text-normal',data: '¡Y mucho más!'},
+    {type: 'img-item',data: 'https://www.bbva.pe/content/dam/public-web/peru/images/cuadros-/cuadro-numero-whatsapp-bbvape.png.img.2400.1578680880932.png',alt: 'img-cel'}
+  ]
+};
+
+const CONTENT_ACCORDION_FOOTER_TERMS = {
+  title: 'Términos y Condiciones',
+  opened: true,
+  items: [
+    {type: 'img-item',data: 'https://www.bbva.pe/content/dam/public-web/peru/images/capsulas-asbanc/agosto-2020/capsula-tarjeta-credito.png',alt: 'img-capsule'},
+    {type: 'text-normal',data: 'TCEA Máxima Referencial 130.09% calculada en base a la tarjeta BBVA Visa y Mastercard Bfree considerando una TEA 84.99% aplicable a un consumo de S/1,000 a 12 meses y cargos que incluyen: envío electrónico de estado de cuenta S/0.00, seguro de gravamen S/5.90 y membresía S/175 cobrada en el mes 12. Puede que la TCEA aplicable a una tarjeta sea menor.'},
+    {type: 'text-bold',data: 'Sobre la oferta de Tarjeta de Crédito:'},
+    {type: 'text-normal',data: 'El otorgamiento de la tarjeta de crédito está sujeto a evaluación crediticia, a la suscripción de los documentos contractuales y a la presentación de documentación exigida por el Banco en caso corresponda. Para mayor información estamos a tu disposición en nuestra Banca por Teléfono al (01) 595-0000 o puedes ingresar a nuestra página Web. Las tasas y tarifas de todos nuestros productos se encuentran publicadas en las oficinas del Banco y en bbva.pe.'},
+    {type: 'text-bold',data: 'Campaña: Te devolvemos 2 meses gratis de Netflix o Spotify:'},
+    {type: 'text-normal',data: 'Campaña válida para las nuevas tarjetas de crédito emitidas por los diferentes canales del banco: red de oficinas, telemarketing y canales digitales del 01 de agosto al 31 de agosto del 2020 a nivel nacional.'},
+    {type: 'text-normal',data: 'Para acceder a la promoción, el cliente deberá afiliar su Tarjeta de Crédito BBVA durante la vigencia de la campaña a Netflix y/o Spotify. No aplica para upgrade, migración, renovación, tarjetas empresariales, corporativas, Capital de Trabajo ni tarjetas de débito. Al cliente se le dará el beneficio de devolverle sus dos primeros cobros o afiliaciones que se generen de las plataformas de entretenimiento: Netflix o Spotify durante tres meses consecutivos, ya sea: agosto – setiembre – octubre o setiembre – octubre – noviembre del 2020. El primer cargo por afiliación a Netflix o Spotify se deberá dar a más tardar el 31 de agosto o 30 de setiembre, respectivamente.'},
+    {type: 'text-normal',data: 'El abono del premio se hará de forma mensual y verificando que el cliente siga afiliado al servicio. Para los que se afiliaron en agosto (el primer cobro es en agosto), la primera devolución será como máximo el 31 de octubre del 2020 y la segunda devolución como máximo el 30 de noviembre del 2020. Para los que se afiliaron en setiembre (el primer cobro es en setiembre), la primera devolución será como máximo el 30 de noviembre del 2020 y la segunda devolución como máximo el 31 de diciembre del 2020. El abono máximo mensual por cliente es S/50 (la devolución dependerá del paquete de Netflix o Spotify que se contrató) y se hará a la tarjeta de crédito donde se verifique la afiliación. Si el cliente afilia su tarjeta de crédito titular o adicional tanto en Netflix como a Spotify, se considerará solo una para el premio y será la de mayor valor. Si el cliente afilia dos o más cuentas de la misma plataforma de entretenimiento a la tarjeta de crédito titular o adicional, solo se considerará una para el premio y será la de mayor valor.'},
+    {type: 'text-normal',data: 'Si el cobro de estos servicios a la tarjeta de crédito es en dólares, el abono del premio será en soles y al tipo de cambio del día del abono. Participa también si la afiliación fue hecha a la tarjeta adicional del titular.'},
+    {type: 'text-normal',data: 'No se realizará el abono del premio a las tarjetas o contratos que se encuentren emitidos fuera de las fechas indicadas, cancelados, bloqueados por mora o sobregiro al momento de las validaciones.'},
+    {type: 'text-normal',data: 'Este premio es adicional al premio de bienvenida que se asigna a todos los clientes que solicitan una nueva tarjeta de crédito y llegan a la meta de consumo establecida para cada una, para más información ingresa a: bbva.pe.'},
+    {type: 'text-normal',data: 'El Banco BBVA actúa como intermediario por lo que no tendrá responsabilidad legal por cualquier daño o perjuicio que pudiera derivarse de la idoneidad de los servicios o productos que ofrecen los establecimientos que participan en la presente campaña. Entérate más en bbva.pe.'},
+    {type: 'text-normal',data: 'Para mayor información estamos a tu disposición en nuestra Banca por Teléfono al (01) 595-0000 o puedes ingresar a nuestra página Web. Las tasas y tarifas de todos nuestros productos se encuentran publicadas en las oficinas del banco y en bbva.pe.'},
+  ]
+};
