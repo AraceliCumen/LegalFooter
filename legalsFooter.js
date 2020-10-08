e3cfdf7a40fd9e13d06a8095317f2fda52fef920
araceli.cueva.contractor <araceli.cueva.contractor@bbva.com>
2020-10-08 16:02:40 -0500

    updates

---
+++ legalsFooter.js
@@ -0,0 +1,34 @@
+window.LEGALS_FOOTER = () => {
+    return (
+      {
+        BENEFITS : {
+            title: "Solicitar Tarjeta de crédito online BBVA",
+            opened: false,
+            items: [
+                {type: "text-normal",data: "Solicita tu Tarjeta de Crédito online BBVA desde la comodidad de tu hogar, desde tu trabajo o donde quiera que estés y en cualquier momento del día. Pide tu tarjeta de crédito online y empieza a disfrutar de todos los beneficios que tenemos para ti:"},
+                {type: "lists-items",data: [{"text": "Descuentos en miles de establecimiento"},{"text": "Compra en cuotas y realiza tus pagos sin intereses en miles de tiendas"},{"text": "Promociones especiales"}]},
+                {type: "text-normal",data: "¡Y mucho más!"},
+                {type: "img-item",data: "https://www.bbva.pe/content/dam/public-web/peru/images/cuadros-/cuadro-numero-whatsapp-bbvape.png.img.2400.1578680880932.png","alt": "img-cel"}
+            ]
+        },
+        TERMS : {
+            title: "Términos y Condiciones",
+            opened: true,
+        items: [
+          {type: 'img-item',data: 'https://www.bbva.pe/content/dam/public-web/peru/images/capsulas-asbanc/octubre-2020/capsula-visa-infinite-oct.svg',alt: 'img-capsule'},
+          {type: 'text-normal',data: 'TCEA Máxima Referencial 130.09% calculada en base a la tarjeta BBVA Visa y Mastercard Bfree considerando una TEA 84.99% aplicable a un consumo de S/1,000 a 12 meses y cargos que incluyen: envío electrónico de estado de cuenta S/0.00, seguro de gravamen S/5.90 y membresía S/175 cobrada en el mes 12. Puede que la TCEA aplicable a una tarjeta sea menor.'},
+          {type: 'text-bold',data: '(1) Sobre la oferta de Tarjeta de Crédito:'},
+          {type: 'text-normal',data: 'El otorgamiento de la tarjeta de crédito está sujeto a evaluación crediticia, a la suscripción de los documentos contractuales y a la presentación de documentación exigida por el Banco en caso corresponda. Para mayor información estamos a tu disposición en nuestra Banca por Teléfono al (01) 595-0000 o puedes ingresar a nuestra página Web. Las tasas y tarifas de todos nuestros productos se encuentran publicadas en las oficinas del Banco y en bbva.pe.'},
+          {type: 'text-normal',data: 'Esta web brinda préstamos con un plazo mínimo de pago de 6 meses y el plazo máximo de 60 meses.'},
+          {type: 'text-normal',data: 'BBVA Sede central, Av. República de Panamá 3055, Lima - Perú.'},
+          {type: 'text-bold',data: '(2) Campaña Te devolvemos S/75'},
+          {type: 'text-normal',data: 'Promoción válida para clientes titulares de las tarjetas de crédito emitidas por los diferentes canales del banco: red de oficinas, telemarketing y canales digitales del 01 de octubre al 31 de octubre del 2020 para nuevas tarjetas. Este premio consiste en una devolución de S/75 y se otorga al cliente titular que cumpla con la meta de consumo mínimo acumulado de S/300 o su equivalente en dólares en cualquier POS o compras por internet durante los primeros 15 días desde la activación de la tarjeta de crédito. '},
+          {type: 'text-normal',data: 'El consumo puede ser en cuotas o sin cuotas y se considerará los consumos que realice la tarjeta de crédito titular y adicional si hubiese. La fecha máxima de activación de la tarjeta de crédito tiene que ser hasta el 15 de noviembre del 2020. Si el cliente no pudo activar su tarjeta de crédito hasta esa fecha, no participara de esta campaña.'},
+          {type: 'text-normal',data: 'El abono del premio se hará a la tarjeta de crédito del titular del contrato. El premio se abonará por única vez al cliente titular y como fecha máxima el 15 de diciembre del 2020. No aplica para upgrade, migración, renovación, reposición, tarjetas empresariales, corporativas ni tarjetas de débito. No es válido los retiros de efectivo, traslados de deuda, pagos en nuestra banca por internet, cargos recurrentes, débitos automáticos ni consumos en casinos. No se realizará el abono a las tarjetas o contratos que se encuentren emitidos fuera de las fechas indicadas, cancelados, bloqueados por mora o sobregiro al momento de las validaciones.'},
+          {type: 'text-normal',data: 'Este premio es adicional al premio de bienvenida que se asigna a todos los clientes que solicitan una nueva tarjeta de crédito y llegan a la meta de consumo establecida para cada una, para más información ingresa a: bbva.pe. '},
+          {type: 'text-normal',data: 'Las transacciones realizadas con Tarjeta Crédito se encuentran sujetas a confirmación por parte del comercio involucrado para ser cargadas, hasta no contar con dicha confirmación, el monto de las transacciones se refleja como "pendiente" en el correspondiente Estado de Cuenta. En ese sentido, BBVA no se responsabiliza por la confirmación de transacciones y demoras en las mismas, siendo éstas exclusiva responsabilidad del comercio elegido por el cliente.'},
+          {type: 'text-normal',data: 'Para mayor información estamos a tu disposición en nuestra Banca por Teléfono al (01) 595-0000 o puedes ingresar a nuestra página Web. Las tasas y tarifas de todos nuestros productos se encuentran publicadas en las oficinas del Banco y en bbva.pe.'}
+        ]
+        }
+    })
+  };
